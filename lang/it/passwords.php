0ec80e1d3ebe86723ea0d56fe9979a1d742a8cfd
Mircha Emanuel `ryuujin` D'Angelo <mirchaemanuel@gmail.com>
2026-01-22 18:28:16 +0100

    translation

---
+++ lang/it/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'La tua password è stata reimpostata.',
+    'sent' => 'Ti abbiamo inviato via email il link per reimpostare la password.',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Questo token per la reimpostazione della password non è valido.',
+    'user' => 'Non riusciamo a trovare un utente con questo indirizzo email.',
+
+];
